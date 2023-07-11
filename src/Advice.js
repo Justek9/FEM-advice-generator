23c9ee9ade39196e43bf3c16ca9165912ce96af3
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-07-11 11:52:33 +0200

    fetch advice

--- src/Advice.js
+++ src/Advice.js
@@ -1,19 +1,31 @@
+import { useState } from 'react'
 import classes from './Advice.module.css'
 import desktop from './img/desktop.svg'
 import dice from './img/dice.svg'
 
 function Advice() {
-	function handleClick() {
-		console.log('clicked')
+	const [advice, setAdvice] = useState('')
+	const [error, setError] = useState(null)
+
+	function fetchAdvice() {
+		const url = `https://api.adviceslip.com/advice`
+		fetch(url)
+			.then(response => {
+				if (!response.ok) throw new Error('Something went wrong with fetching advice... Please try again')
+				else return response.json()
+			})
+			.then(data => setAdvice(data.slip.advice))
+			.catch(error => setError(error.message))
 	}
 	return (
 		<div className={classes.main}>
 			<div className={classes.container}>
 				<p className={classes.advice}>Advice # 1</p>
-				<p>"Lorem ipsum"</p>
+				{error}
+				{!error && <p className={classes.text}>"{advice}"</p>}
 				<img src={desktop} alt='line'></img>
 			</div>
-			<div onClick={handleClick} className={classes.circle}>
+			<div onClick={fetchAdvice} className={classes.circle}>
 				<img src={dice} alt='dice'></img>
 			</div>
 		</div>
